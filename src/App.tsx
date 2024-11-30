8e842cff274795037002a60d39ae07db71e74b2b
Azu11 <toquicaagudelo123@gmail.com>
2024-11-30 14:17:37 -0500

    fix app create user route

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,14 @@ import { CreateUser } from "@/pages/User";
 import { store, persistor } from "./redux/store";
 import { Spinner } from "@/components";
 import { useCheckBackend } from "@/hooks/useCheckBackend";
+import { useNavigate } from "react-router-dom";
 
 const Login = lazy(() => import("@/pages/Login/Login"));
 
 function App() {
   const { backendReady, loading } = useCheckBackend();
+
+  const navigate = useNavigate()
 
   const queryClient = new QueryClient()
 
@@ -56,7 +59,17 @@ function App() {
                 <Route element={<AdminLayout />}>
                   <Route path="/dashboard" element={<Dashboard />} />
                   <Route path="/create-category" element={<CreateCategory />} />
-                  <Route path="/create-user" element={<CreateUser />} />
+                  <Route
+                      path="/create-user"
+                      element={
+                        <CreateUser
+                          onUserCreated={(user) => {
+                            console.log("Usuario creado:", user);
+                            navigate("/users");
+                          }}
+                        />
+                      }
+                    />
                   <Route path="/users" element={<UserPage />} />
                   <Route path="/rol" element={<RolePage />} />
                 </Route>
