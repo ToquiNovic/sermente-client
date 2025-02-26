c145fcdfebf0e09f2884cbb84b2a1c753d560a7a
ToquiNovic <toquicaagudelo123@gmail.com>
2025-02-25 19:14:15 -0500

    add filter numberDoc

--- src/pages/User/DataTable/data-table.tsx
+++ src/pages/User/DataTable/data-table.tsx
@@ -1,9 +1,11 @@
 // pages/User/DataTable/data-table.tsx
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   useReactTable,
   getPaginationRowModel,
 } from "@tanstack/react-table";
@@ -17,6 +19,8 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+import { Input } from "@/components/ui/input"
+
 import { DataTablePagination } from "@/components/app/DataTablePagination";
 
 interface DataTableProps<TData> {
@@ -27,16 +31,34 @@ interface DataTableProps<TData> {
 const DataTable = <TData,>({ columns, data }: DataTableProps<TData>) => {
   const memoizedData = useMemo(() => data, [data]);
   const memoizedColumns = useMemo(() => columns, [columns]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
+    []
+  )
 
   const table = useReactTable({
     data: memoizedData,
     columns: memoizedColumns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      columnFilters,
+    },
   });
 
   return (
     <div>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Buscar Usuario..."
+          value={(table.getColumn("numberDoc")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("numberDoc")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
       <div className="rounded-md border">
         <Table>
           <TableHeader>
